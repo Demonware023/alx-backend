c4eb973143845860d6258709ec0ff3df89bb1d86
Demonware023 <awamchimere@icloud.com>
2024-08-24 19:49:14 -0700

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" LRUCache module
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache defines a LRU caching system
+    """
+
+    def __init__(self):
+        """ Initialize the class
+        """
+        super().__init__()
+        self.access_order = []  # List to keep track of the order of access
+
+    def put(self, key, item):
+        """ Add an item in the cache using LRU algorithm
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            # If the key is already in the cache, remove it from its current position
+            self.access_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # If the cache is full, remove the least recently used item
+            lru_key = self.access_order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+
+        # Add the new key-value pair and update the access order
+        self.cache_data[key] = item
+        self.access_order.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Move the accessed key to the end to show it was recently used
+        self.access_order.remove(key)
+        self.access_order.append(key)
+        return self.cache_data[key]
