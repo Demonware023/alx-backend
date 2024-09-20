d1f30177c651a07ba1f6d920e8a17f5e253ea516
Demonware023 <awamchimere@icloud.com>
2024-09-20 09:18:34 +0000

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,55 +1,27 @@
 #!/usr/bin/env python3
-"""
-Flask app with Babel for internationalization and language selection.
-"""
+""" 4-app module """
+from typing import Union
+from flask import Flask, request
+from flask_babel import Babel
+from config import Config
+from routes.routes_4 import app_routes
 
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
 
 app = Flask(__name__)
-
-
-class Config:
-    """
-    Configuration class for Flask-Babel.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+babel = Babel(app)
 
 app.config.from_object(Config)
-
-babel = Babel(app)
+app.register_blueprint(app_routes)
 
 
 @babel.localeselector
-def get_locale():
-    """
-    Determines the best match with supported languages based on
-    the request's Accept-Language header or the locale parameter in the URL.
-
-    Returns:
-        str: The best match language code.
-    """
-    # Check if the locale parameter is in the request args and is supported
+def get_locale() -> Union[str, None]:
+    """ get locale """
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in Config.LANGUAGES:
         return locale
-    # Fall back to the best match based on the Accept-Language header
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index() -> str:
-    """
-    Handles the index route and renders the index template.
-
-    Returns:
-        str: Rendered HTML template.
-    """
-    return render_template('3-index.html')
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port="5000")
